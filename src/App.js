5132d0eb08e09170455382847cc214af226c8e25
Your Name <you@example.com>
2023-05-13 18:48:46 +0530

    this is first e commerce

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./style/app.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Cart from "./components/Cart";
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  return (
+    <Router>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />}/>
+      </Routes>
+      <Toaster />
+    </Router>
+  );
+}
+
+export default App;
